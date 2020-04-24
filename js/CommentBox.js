ba38e4db8154d685f9ced55d8fcbd6507cd9bc99
Daniel <32684450+Daniel-Ronson@users.noreply.github.com>
2020-04-23 23:29:23 -0700

    customized css and content

---
+++ js/CommentBox.js
@@ -0,0 +1,13 @@
+var CommentBox = React.createClass({
+  displayName: "CommentBox",
+
+  render: function render() {
+    return React.createElement(
+      "div",
+      { className: "commentBox" },
+      "Hello, world! I am a CommentBox."
+    );
+  }
+});
+
+ReactDOM.render(React.createElement(CommentBox, null), document.getElementById('container'));
